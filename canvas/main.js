d1cdd33ed6753c89ff38b22bf1dbd99f9e72fb71
kitaminka <kit4minka@gmail.com>
2023-09-20 22:58:51 +0300

    Update canvas

--- canvas/main.js
+++ canvas/main.js
@@ -1,52 +1,75 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const border = 10;
-const maxStep = 50;
+const colorChange = 10;
+const shadowDisplacement = 0.08;
 
-let x, y, stepX, stepY;
+const maxGenomeByte = 255;
 
-draw();
+const startX = 500;
+const startY = 800;
 
-function draw() {
+const segmentDisplacement = 2;
+
+const levelCount = 4;
+const paramCount = 3;
+
+const maxLength = 100;
+const minLength = 10;
+
+const maxSize = 40;
+const minSize = 10;
+
+const maxSizeChanges = 0.3;
+const minSizeChanges = -0.3;
+
+let genome = [
+    255, 200, 0,
+    255, 140, 125,
+    200, 0, 125,
+    2, 3, 10
+];
+
+function drawCircle(x, y, radius, rColor, gColor, bColor) {
+    if (radius < 0) {
+        radius = -radius
+    }
     ctx.beginPath();
-    stepX = maxStep
-    x = Math.random() * canvas.width;
-    y = Math.random() * canvas.height;
-    ctx.lineTo(x, y);
-    for (let i = 0; i < 1000; i++) {
-        if (i % 2 === 0) {
-            stepX = Math.random() * maxStep;
-            ctx.strokeStyle = '#33ff33';
-        } else {
-            stepX = -(Math.random() * maxStep);
-            ctx.strokeStyle = '#ff3333';
-        }
-        // stepX = Math.random() * maxStep - maxStep / 2;
-        stepY = Math.random() * maxStep - maxStep / 2;
-        // ctx.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-        // if (stepX > 20) {
-        //     ctx.strokeStyle = '#33ff33';
-        // } else {
-        //     ctx.strokeStyle = '#ff3333';
-        // }
-        ctx.moveTo(x, y);
-        x += stepX;
-        y += stepY;
-        if (y > canvas.height - border) {
-            y = canvas.height - border;
-        }
-        if (x > canvas.width - border) {
-            x = canvas.width - border;
-        }
-        if (y < border) {
-            y = border;
-        }
-        if (x < border) {
-            x = border;
-        }
-        ctx.lineTo(x, y);
-        ctx.stroke();
-        ctx.beginPath();
+    ctx.fillStyle = `#${(rColor - colorChange).toString(16).padStart(2, '0')}${(gColor - colorChange).toString(16).padStart(2, '0')}${(bColor - colorChange).toString(16).padStart(2, '0')}`;
+    ctx.arc(x-radius*shadowDisplacement, y-radius*shadowDisplacement, radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = `#${(rColor + colorChange).toString(16).padStart(2, '0')}${(gColor + colorChange).toString(16).padStart(2, '0')}${(bColor + colorChange).toString(16).padStart(2, '0')}`;
+    ctx.arc(x+radius*shadowDisplacement, y+radius*shadowDisplacement, radius, 0, 2*Math.PI);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = `#${rColor.toString(16).padStart(2, '0')}${gColor.toString(16).padStart(2, '0')}${bColor.toString(16).padStart(2, '0')}`;
+    ctx.arc(x, y, radius, 0, 2*Math.PI);
+    ctx.fill();
+}
+
+let x = startX;
+let y = startY;
+let turn = 0;
+
+for (let i = 0; i < levelCount; i++) {
+    drawLevel(genome.slice(i*paramCount, i*paramCount+paramCount));
+    turn += 1;
+
+}
+
+function drawLevel(params) {
+    console.log(`drawing ${params}`);
+
+    const length = params[0] / (maxGenomeByte / (maxLength - minLength)) + minLength;
+    let size = params[1] / (maxGenomeByte / (maxSize - minSize)) + minSize;
+    const sizeChanges = params[2] / (maxGenomeByte / (maxSizeChanges - minSizeChanges)) + minSizeChanges;
+
+    for (let i = 0; i < length; i++) {
+        drawCircle(x, y, size, 200, 200, 200);
+        size += sizeChanges;
+        y -= segmentDisplacement;
+        y -= turn;
+        y -= turn;
     }
 }
